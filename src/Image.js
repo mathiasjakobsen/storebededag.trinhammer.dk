de65e24cdb3eeec20af584f8e719ea4ffc8a8e02
Mathias Jakobsen <mathiasjakobsen@me.com>
2019-11-27 21:04:32 +0100

    Create Trinhammer Træf website

---
+++ src/Image.js
@@ -0,0 +1,97 @@
+// @flow
+import * as React from "react"
+import Imgix from "react-imgix"
+import { Waypoint } from "react-waypoint"
+
+const { useState } = React
+
+type Props = {
+  aspectRatio: number,
+  camera: string,
+  fStop: number,
+  focalLength: string,
+  iso: number,
+  name: string,
+  speed: string,
+}
+
+// Placeholder element for images pending load
+const placeholder = <div role="presentation" className="image__img" />
+
+function Image(props: Props): React.Node {
+  const [imageLoaded, setImageLoaded] = useState(false)
+  const [onScreen, setOnScreen] = useState(false)
+
+  // Use local images for development
+  let url = `https://dephotos.imgix.net/${props.name}`
+  if (
+    process.env.PUBLIC_URL !== undefined &&
+    process.env.NODE_ENV &&
+    process.env.NODE_ENV.toUpperCase() === "DEVELOPMENT"
+  ) {
+    url = `${process.env.PUBLIC_URL || ""}/images/${props.name}`
+  }
+
+  const imgClass = [
+    "image__img",
+    // Controls transition when the image is in view and loaded
+    imageLoaded && onScreen ? "is-loaded" : "is-not-loaded",
+  ].join(" ")
+
+  const image = (
+    <Imgix
+      src={url}
+      sizes={`calc((var(--imgHeight)) * ${props.aspectRatio})`}
+      htmlAttributes={{
+        loading: "lazy",
+        onLoad: () => setImageLoaded(true),
+      }}
+      className={imgClass}
+    />
+  )
+
+  // The image name will be used as the unique key
+  const imageName = props.name.split(".")[0]
+
+  let speed = null
+  if (props.speed !== 'undefined') {
+    speed =
+      // If the shutter speed is a fraction, we want to style it appropriately.
+      String(props.speed).indexOf("/") === -1 ? (
+        <span>{props.speed} sec, </span>
+      ) : (
+        <span className="frac">{props.speed} sec, </span>
+      )
+  }
+
+  return (
+    <div id={imageName} className="pane page--image">
+      <Waypoint
+        key={imageName}
+        horizontal={true}
+        topOffset="-200%"
+        bottomOffset="0"
+        onEnter={() => setOnScreen(true)}
+        onLeave={() => setOnScreen(false)}
+      />
+      <div
+        className="pane__image u-mb0"
+        style={{
+          minWidth: `calc((var(--imgHeight)) * ${props.aspectRatio})`,
+        }}
+      >
+        {onScreen ? image : placeholder}
+      </div>
+      <p className="image__info u-mb0">
+        <b>{props.name.split('.').slice(0, -1).join('.')} </b>
+          {props.camera && `${props.camera}, `}
+          {props.fStop && `\u0192${props.fStop}, `}
+          {speed}
+          {props.focalLength && `${props.focalLength}, `}
+          <span className="caps">ISO</span>{" "}{props.iso}
+      </p>
+    </div>
+  )
+}
+
+export default Image
